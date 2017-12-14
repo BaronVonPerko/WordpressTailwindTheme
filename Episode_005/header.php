c1a3c8e26fc138cef66f2b3881bfdb60e8d27357
Chris Perko <Chris@chrisperko.net>
2017-12-14 16:09:51 -0600

    Episode 005

---
+++ Episode_005/header.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Tailwind_Theme
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content',
+			'tailwindtheme' ); ?></a>
+
+    <header id="masthead"
+            class="py-4 px-2 bg-grey-lightest shadow mb-2 flex flex-col md:flex-row
+            text-center md:text-left
+            justify-between items-center border-t-4 border-teal">
+        <div class="flex items-center">
+			<?php the_custom_logo(); ?>
+
+            <div class="ml-4">
+                <h1 class="mb-2">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+                    </a>
+                </h1>
+
+				<?php
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+                    <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+					<?php
+				endif; ?>
+            </div>
+        </div><!-- .site-branding -->
+
+        <nav id="site-navigation" class="mt-4 md:mt-0">
+            <button class="sm:hidden mb-4" aria-controls="primary-menu" aria-expanded="false">
+	            <?xml version="1.0" ?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>
+            </button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+                'container' => 'ul',
+                'menu_class' => 'list-reset hidden sm:flex text-lg sm:li-pl-4 li-pb-4 md:li-pb-0'
+			) );
+			?>
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->wp_nav_menu
+
+    <div id="content" class="site-content">
