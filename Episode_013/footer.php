66185a38034277679cc1e308068dfc886f711bda
Chris Perko <chris@chrisperko.net>
2018-02-06 17:45:47 -0600

    Episode 13

---
+++ Episode_013/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Tailwind_Theme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer h-64 bg-teal flex flex-col justify-center items-center">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tailwindtheme' ) ); ?>"><?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'tailwindtheme' ), 'WordPress' );
+			?></a>
+			<span class="sep"> | </span>
+			<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tailwindtheme' ), 'tailwindtheme', '<a href="http://ChrisPerko.net">Chris Perko</a>' );
+			?>
+		</div><!-- .site-info -->
+        <div class="mt-4">
+            <?php require get_stylesheet_directory() . '/inc/social-icon-links.php' ?>
+        </div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
